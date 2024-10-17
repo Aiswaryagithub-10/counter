1266a38bf241c8b8b3e892b611880ab6ee55bafc
Aiswaryagithub-10 <aiswarya.p.b10@gmail.com>
2024-10-18 04:28:54 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Counter from './Counter';
+import Shopping from './Shopping';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Counter />
-  </React.StrictMode>
+ <Shopping/>
 );
